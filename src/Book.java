018e9dcf32f49d9e7c883d3aef4d6c40c290376e
Maximfsda <lisitsin.maxi@yandex.ru>
2022-10-05 23:00:43 +1000

    Hw

---
+++ src/Book.java
@@ -0,0 +1,36 @@
+public class Book {
+
+   private String name;
+   private Author author;
+   private int yearPublication;
+
+   public Book(String name, Author author, int yearPublication) {
+      this.name = name;
+      this.author = author;
+      this.yearPublication = yearPublication;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public Author getAuthor() {
+      return author;
+   }
+
+   public int getYearPublication() {
+      return yearPublication;
+   }
+
+   public void setYearPublication(int yearPublication) {
+      this.yearPublication = yearPublication;
+   }
+   @Override
+   public String toString() {
+      return author.getName()+ " " + author.getSurname() + ": " + name + ": " + yearPublication;
+   }
+
+
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) {
+        Author author = new Author("Tim", "Berthon");
+        Author authorOne = new Author("Slim", "Wigen");
+        Book bookOne = new Book("Derma", author, 1999);
+        Book bookTwo = new Book("Desrma", authorOne, 1929);
+        bookOne.setYearPublication(2005);
+        bookTwo.setYearPublication(2000);
 
+        System.out.println("Средний уровень");
+        Book[] myBook = new Book[5];
+        bookAddition(myBook, bookOne);
+        bookAddition(myBook, bookTwo);
+        allPrint(myBook);
+    }
+
+    public static void bookAddition(Book[] myBook, Book book) {
+        for (int i = 0; i < myBook.length; i++) {
+            if (myBook[i] == null) {
+                myBook[i] = book;
+                break;
+            }
+        }
+    }
+
+    public static void allPrint(Book[] myBook) {
+        for (int i = 0; i < myBook.length; i++) {
+            if (myBook[i] != null) {
+                System.out.println(myBook[i]);
+            }
+        }
+    }
 }
